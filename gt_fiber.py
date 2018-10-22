bcf854cf6e5a287d0d56204f2c4ad39b9f321a8e
Po-Yu Kao <poyu_kao@ucsb.edu>
2018-10-21 17:46:51 -0700

    use roi

--- gt_fiber.py
+++ gt_fiber.py
@@ -86,11 +86,12 @@ for idx, whole_tumor_file in enumerate(whole_tumor_files_dir):
     pat_name = whole_tumor_file[:whole_tumor_file.find('.nii.gz')]
     print(idx, pat_name)
     roi = '--roi='+os.path.join(whole_tumor_dir, whole_tumor_file)
-    seed = '--seed='+os.path.join(whole_tumor_dir, whole_tumor_file)
+    #seed = '--seed='+os.path.join(whole_tumor_dir, whole_tumor_file)
     connectivity_type = '--connectivity_type=end'
     connectivity_value = '--connectivity_value=count'
     connectivity_threshold = '--connectivity_threshold=0'
-    subprocess.call(['./dsi_studio', '--action=trk', '--source='+source, seed, roi, parameter_id, '--output=no_file', '--connectivity=aal', connectivity_type, connectivity_value, connectivity_threshold])
+    subprocess.call(['./dsi_studio', '--action=trk', '--source='+source, roi, parameter_id, '--output=no_file', '--connectivity=aal', connectivity_type, connectivity_value, connectivity_threshold])
+    #subprocess.call(['./dsi_studio', '--action=trk', '--source='+source, seed, roi, parameter_id, '--output=no_file', '--connectivity=aal', connectivity_type, connectivity_value, connectivity_threshold])
 
     network_measure_files = [os.path.join(root, name) for root, dirs, files in os.walk(work_dir) for name in files if 'network_measures' in name and name.endswith('.txt')]
     network_measure_file_dst = os.path.join(os.path.split(network_measure_files[0])[0], 'network_measures', 'gt', os.path.split(network_measure_files[0].replace(source, pat_name))[1])
@@ -110,11 +111,12 @@ for idx, whole_tumor_file in enumerate(whole_tumor_files_dir):
     pat_name = whole_tumor_file[:whole_tumor_file.find('.nii.gz')]
     print(idx, pat_name)
     roi = '--roi='+os.path.join(whole_tumor_dir, whole_tumor_file)
-    seed = '--seed='+os.path.join(whole_tumor_dir, whole_tumor_file)
+    #seed = '--seed='+os.path.join(whole_tumor_dir, whole_tumor_file)
     connectivity_type = '--connectivity_type=pass'
     connectivity_value = '--connectivity_value=count'
     connectivity_threshold = '--connectivity_threshold=0'
-    subprocess.call(['./dsi_studio', '--action=trk', '--source='+source, seed, roi, parameter_id, '--output=no_file', '--connectivity=aal', connectivity_type, connectivity_value, connectivity_threshold])
+    subprocess.call(['./dsi_studio', '--action=trk', '--source='+source, roi, parameter_id, '--output=no_file', '--connectivity=aal', connectivity_type, connectivity_value, connectivity_threshold])
+    #subprocess.call(['./dsi_studio', '--action=trk', '--source='+source, seed, roi, parameter_id, '--output=no_file', '--connectivity=aal', connectivity_type, connectivity_value, connectivity_threshold])
 
     network_measure_files = [os.path.join(root, name) for root, dirs, files in os.walk(work_dir) for name in files if 'network_measures' in name and name.endswith('.txt')]
     network_measure_file_dst = os.path.join(os.path.split(network_measure_files[0])[0], 'network_measures', 'gt', os.path.split(network_measure_files[0].replace(source, pat_name))[1])
